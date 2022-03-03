381acb15144680ac4541721e803bdc02e0b583f3
Nivo1985 <karol.rogowski@gmail.com>
2022-03-03 21:40:57 +0100

    Add files via upload

---
+++ Sem2/Zjazd 1/10)Syntax/3)Functions.js
@@ -0,0 +1,51 @@
+//functions
+
+// function declarationFunc(){
+//     console.log('declarationFunc');
+// }
+// declarationFunc();
+
+
+// declarationFunc();
+// function declarationFunc(){
+//     console.log('declarationFunc');
+// }
+
+
+// var expresionFunc = function(){
+//     console.log('expresionFunc');
+// };
+// expresionFunc();
+
+// expresionFunc();
+// var expresionFunc = function(){
+//     console.log('expresionFunc');
+// };
+
+
+// var outName = function inName() {
+//     console.log(inName);
+// };
+// outName();
+// console.log(inName);
+
+
+// askQuestion();
+// let studentName = "Suzy";
+// function askQuestion() {
+//     console.log(`${ studentName }, what do you think?`);
+// }
+
+
+// askQuestion();
+// var studentName = "Suzy";
+// function askQuestion() {
+//     console.log(`${ studentName }, what do you think?`);
+// }
+
+
+// let studentName = "Suzy";
+// askQuestion();
+// function askQuestion() {
+//     console.log(`${ studentName }, what do you think?`);
+// }
